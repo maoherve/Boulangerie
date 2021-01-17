f4741f9f38e84ac3dc80bf9ba90431c38162a117
Mao Mater <maoherve8@gmail.com>
2021-01-17 10:54:08 +0100

    pruduct page done

--- src/Controller/HproductsController.php
+++ src/Controller/HproductsController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ * @Route("/products", name="products_")
+ */
 class HproductsController extends AbstractController
 {
     /**
-     * @Route("/products", name="products")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
+        $products = $this->getDoctrine()
+            ->getRepository(Products::class)
+            ->findAll();
+
         return $this->render('hproducts/index.html.twig', [
-            'controller_name' => 'HproductsController',
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/productDetails/{id}", name="Details")
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Products::class)
+            ->find($id);
+
+
+
+        return $this->render('hproducts/productDetails/productDetails.html.twig', [
+            'product' => $product,
         ]);
     }
 }

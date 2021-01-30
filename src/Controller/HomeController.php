00fe4812f3c6c37f194df71f6ff58553ffe80377
Mao Mater <maoherve8@gmail.com>
2021-01-31 00:23:39 +0100

    crud home texte + cohérence admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Carousel;
+use App\Entity\HomeTexte;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,8 +21,13 @@ class HomeController extends AbstractController
             ->getRepository(Carousel::class)
             ->findAll();
 
+        $homeTexte = $this->getDoctrine()
+            ->getRepository(HomeTexte::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
             'carousel' => $carousel,
+            'homeTexte' => $homeTexte,
         ]);
     }
 }
